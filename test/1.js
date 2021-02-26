ef925c419cfa30c406e47ca0b7165b9f3917f5fe
jswj30 <jswj30@naver.com>
2021-02-26 21:14:30 +0900

    1

--- test/1.js
+++ test/1.js
@@ -28,4 +28,11 @@ function exampleCubic(n) {
   }
 }
 
-exampleCubic(3);
+// O(logn)
+function exampleLogarithmic(n) {
+  for (var i = 2; i <= n; i = i * 2) {
+    console.log(i);
+  }
+}
+
+exampleLogarithmic(16);
