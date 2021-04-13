47ad177a66fc7d13c146ac47bb188396dde8772c
jswj30 <jswj30@naver.com>
2021-04-13 14:38:43 +0900

    1

---
+++ test/1_big-o.js
@@ -0,0 +1,51 @@
+// O(1)
+function oone(n) {
+  console.log(n);
+}
+
+// oone(5);
+
+// O(n)
+
+function exampleLinear(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log("exampleLinear: ", i);
+  }
+}
+// exampleLinear(5);
+
+// O(n^2)
+
+function exampleQuadratic(n) {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+    for (let j = i; j < n; j++) {
+      console.log(j);
+    }
+  }
+}
+// exampleQuadratic(3);
+
+// O(n^3)
+
+function exampleCubic(n) {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+    for (let j = i; j < n; j++) {
+      console.log(j);
+      for (let k = j; k < n; k++) {
+        console.log(k);
+      }
+    }
+  }
+}
+// exampleCubic(3);
+
+// O(log n)
+
+function exampleLogarithmic(n) {
+  for (let i = 2; i <= n; i = i * 2) {
+    console.log(i);
+  }
+}
+// exampleLogarithmic(100);
