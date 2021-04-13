04bc57c7317be2ed9135d969f789dbc314f78e0a
jswj30 <jswj30@naver.com>
2021-04-13 15:28:52 +0900

    1

--- test/1_big-o.js
+++ test/1_big-o.js
@@ -49,3 +49,32 @@ function exampleLogarithmic(n) {
   }
 }
 // exampleLogarithmic(100);
+
+// 계수 법칙
+
+// function a(n) {
+//   let count = 0;
+//   for (let i = 0; i < n; i++) {
+//     count += 1;
+//   }
+//   return count;
+// }
+
+// function a(n) {
+//   let count = 0;
+//   for (let i = 0; i < 5 * n; i++) {
+//     count += 1;
+//   }
+//   return count;
+// }
+
+function a(n) {
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    count += 1;
+  }
+  count += 3;
+  return count;
+}
+
+console.log(a(5));
