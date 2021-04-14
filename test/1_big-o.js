98170112c34a440187a96ebf604cdb56daf6f286
jswj30 <jswj30@naver.com>
2021-04-14 11:47:27 +0900

    1

--- test/1_big-o.js
+++ test/1_big-o.js
@@ -96,12 +96,83 @@ function exampleLogarithmic(n) {
 
 // 다항 법칙
 
-function a(n) {
-  let count = 0;
-  for (let i = 0; i < n * n; i++) {
-    count += 1;
+// function a(n) {
+//   let count = 0;
+//   for (let i = 0; i < n * n; i++) {
+//     count += 1;
+//   }
+//   return count;
+// }
+
+// console.log(a(5));
+
+//---------------------------------------
+
+// 연습 문제
+
+// 문제 1
+function someFunction(n) {
+  for (let i = 0; i < n * 1000; i++) {
+    for (let j = 0; j < n * 20; j++) {
+      console.log(i + j);
+    }
   }
-  return count;
 }
 
-console.log(a(5));
+// O(n^2)
+
+// 문제 2
+
+function someFunction(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      for (let k = 0; k < n; k++) {
+        for (let l = 0; l < 10; l++) {
+          console.log(i + j + k + l);
+        }
+      }
+    }
+  }
+}
+
+// O(n^3)
+
+// 문제 3
+
+function someFunction(n) {
+  for (let i = 0; i < 1000; i++) {
+    console.log("hi");
+  }
+}
+
+// O(1)
+
+// 문제 4
+
+function someFunction(n) {
+  for (let i = 0; i < 10 * n; i++) {
+    console.log(n);
+  }
+}
+
+// O(logn)
+
+// 문제 5
+
+function someFunction(n) {
+  for (let i = 1; i < n; i = i * 2) {
+    console.log(n);
+  }
+}
+
+// O(log 2n)
+
+// 문제 6
+
+function someFunction(n) {
+  while (true) {
+    console.log(n);
+  }
+}
+
+// 무한?
