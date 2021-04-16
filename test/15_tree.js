69cc0b6aafe192e3d5db5cf473c63e0e10ac95a7
jswj30 <jswj30@naver.com>
2021-04-16 14:30:24 +0900

    binaryTree

--- test/15_tree.js
+++ test/15_tree.js
@@ -9,6 +9,14 @@ let test1 = new TreeNode(1);
 let test2 = new TreeNode(2);
 test1.children.push(test2);
 
-console.log("test1: ", test1);
-console.log("test1.value: ", test1.value);
-console.log("test1.children: ", test1.children);
+// console.log("test1: ", test1);
+// console.log("test1.value: ", test1.value);
+// console.log("test1.children: ", test1.children);
+
+// 이진 트리
+
+function BinaryTreeNode(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
