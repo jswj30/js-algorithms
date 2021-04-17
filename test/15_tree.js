685f4c699796e4191efd421d6e35f2a2e00db4b6
jswj30 <jswj30@naver.com>
2021-04-17 22:34:52 +0900

    traverseLevelOrder

--- test/15_tree.js
+++ test/15_tree.js
@@ -206,3 +206,40 @@ BinaryTree.prototype.traversePostOrderIterative = function () {
 // console.log(root1);
 // console.log(root1.traversePostOrder());
 // console.log(root1.traversePostOrderIterative());
+
+// 단계순위 순회
+BinaryTree.prototype.traverseLevelOrder = function () {
+  let root = this._root,
+    queue = [];
+
+  if (!root) return;
+  queue.push(root);
+
+  while (queue.length) {
+    let temp = queue.shift();
+    console.log(temp.value);
+    if (temp.left) queue.push(temp.left);
+    if (temp.right) queue.push(temp.right);
+  }
+};
+
+// 테스트
+
+let root1 = new BinaryTree();
+let leaf1 = new BinaryTreeNode(42);
+let leaf2_1 = new BinaryTreeNode(41);
+let leaf2_2 = new BinaryTreeNode(50);
+leaf1.left = leaf2_1;
+leaf1.right = leaf2_2;
+let leaf3_1 = new BinaryTreeNode(10);
+let leaf3_2 = new BinaryTreeNode(40);
+let leaf3_3 = new BinaryTreeNode(45);
+let leaf3_4 = new BinaryTreeNode(75);
+leaf2_1.left = leaf3_1;
+leaf2_1.right = leaf3_2;
+leaf2_2.left = leaf3_3;
+leaf2_2.right = leaf3_4;
+root1._root = leaf1;
+
+console.log(root1);
+console.log(root1.traverseLevelOrder());
