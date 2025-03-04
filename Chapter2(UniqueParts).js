093dcf6f0c750ef5d7018c7e8d73906740920a51
jswj30 <jswj30@naver.com>
2025-03-04 21:55:31 +0900

    1

--- Chapter2(UniqueParts).js
+++ Chapter2(UniqueParts).js
@@ -129,30 +129,6 @@ function isEquivalent(a, b) {
   return true;
 }
 
-// function isEquivalent(a, b) {
-//     // arrays of property names
-//     var aProps = Object.getOwnPropertyNames(a);
-//     var bProps = Object.getOwnPropertyNames(b);
-
-//     // If their property lengths are different, they're different objects
-//     if (aProps.length != bProps.length) {
-//         return false;
-//     }
-
-//     for (var i = 0; i < aProps.length; i++) {
-//         var propName = aProps[i];
-
-//         // If the values of the property are different, not equal
-//         if (a[propName] !== b[propName]) {
-//             return false;
-//         }
-//     }
-
-//     // If everything matched, correct
-//     return true;
-// }
-// isEquivalent({'hi':12},{'hi':12}); // returns true
-
 var obj1 = { prop1: "test", prop2: function () {} };
 var obj2 = { prop1: "test", prop2: function () {} };
 
